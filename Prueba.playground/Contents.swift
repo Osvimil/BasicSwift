2fbdb4d1f8f20e93e491f51a519bc8d3ba568c50
oswaldo <oswaldoadidas@hotmail.com>
2017-01-19 10:47:29 -0600

    my first examples about using swift

---
+++ Prueba.playground/Contents.swift
@@ -0,0 +1,52 @@
+//: Playground - noun: a place where people can play
+
+//Soy un simple comentario
+
+/*
+ Soy un comentario extenso
+ y necesito ocupar
+ mas de una línea para
+ expresar algo.
+ */
+
+import UIKit
+
+var str = "soy variable cadena implicita"
+
+var cadena: String = "variable cadena explicita"
+var numero: Int = 23
+var nombre = "Soy oswaldo y tengo"
+
+nombre = "Soy Oswaldo Peralta Saldívar y tengo"
+
+let PI = 3.14159
+let cadenin = "SEP"
+
+print(str)
+print(cadena)
+print(nombre+" \(numero) "+"anios")
+
+
+var arreglo = [10,12,14,16,18,20,23]
+
+arreglo.append(25)
+arreglo.append(26)
+
+print(arreglo)
+
+print(arreglo.count)
+
+print(arreglo.isEmpty)
+
+print(arreglo[4])
+print(arreglo[2])
+print(arreglo[5])
+print(arreglo[1])
+print(arreglo[0])
+print(arreglo[6])
+print(arreglo[3])
+
+print(PI)
+print(cadenin)
+
+
